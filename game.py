2b5e2fd360eb39250b40ac14d5d5f5c8b032a5db
Nabil El Bajdi <nabilelbajdi@hotmail.com>
2023-05-26 11:52:22 +0000

    Added clear_screen function

--- game.py
+++ game.py
@@ -1,6 +1,20 @@
-# Prints welcome message when starting the game
+# Prints welcome message and descsription of the game when starting it
+import os
+
 def start_game():
-    print("Welcome to the Text Adventure Game!")
+    print("Welcome to the Text Adventure Game! This is an interactive text-based adventure game that will take you through a journey.")
+    input("Press Enter to start the game ")
+
+def clear_screen():
+    # Clear the terminal screen after the welcome message above
+    if os.name == 'posix':  # For UNIX/Linux/MacOS
+        os.system('clear')
+    elif os.name == 'nt':  # For Windows
+        os.system('cls')
+    else:
+        # For unsupported operating systems, it will do nothing.
+        pass
 
 # Calling the function
 start_game()
+clear_screen()
