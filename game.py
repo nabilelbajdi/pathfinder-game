f0257987286641cb89a6cb3cd5b7f07dba7ca2bb
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2023-05-26 15:19:21 +0200

    Created typerwriter_print function to give a typewriter effect to print statements

--- game.py
+++ game.py
@@ -1,8 +1,18 @@
-# Prints welcome message and descsription of the game when starting it
 import os
+import sys
+import time
 
+# Function to give a typewriter effect for print statements
+def typewriter_print(text):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.03)  # Typing speed
+    print()  # This will move to the next line after printing the whole text
+
+# Prints welcome message and descsription of the game when starting it
 def start_game():
-    print("Welcome to the Text Adventure Game! This is an interactive text-based adventure game that will take you through a journey.")
+    typewriter_print("Welcome to the Text Adventure Game! This is an interactive text-based adventure game that will take you through a journey.")
     input("Press Enter to start the game ")
 
 def clear_screen():
