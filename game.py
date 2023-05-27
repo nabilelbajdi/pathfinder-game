db0bdc9b807ba00c0dfc8e84f44fe8c130d436db
nabilelbajdi <113439816+nabilelbajdi@users.noreply.github.com>
2023-05-27 19:59:10 +0200

    Added act2_beach story and choices. Also added some QoL updates

--- game.py
+++ game.py
@@ -40,15 +40,19 @@ def act1():
         choice = input("Which path do you choose? (forest/beach/cave) ")
 
         if choice == "forest":
+            clear_screen()
             act2_forest()
             break
         elif choice == "beach":
+            clear_screen()
             act2_beach()
             break
         elif choice == "cave":
+            clear_screen()
             act2_cave()
             break
         else:
+            clear_screen()
             print("Invalid choice. Please choose either 'forest', 'beach' or 'cave'.")
 
 def act2_forest():
@@ -70,22 +74,26 @@ def moon_wolf_quest():
     path_choice = input("Which path do you choose? (illuminated/dark) ")
 
     if path_choice == "illuminated":
+        clear_screen()
         typewriter_print("You follow the illuminated path, guided by the moon's gentle glow.")
         typewriter_print("As you venture deeper into the forest, you suddenly hear a familiar howl.")
         typewriter_print("You realize that you have unknowingly circled back to the Moon Wolf.")
         typewriter_print("The Moon Wolf acknowledges your efforts and rewards you with the luminescent mushroom.")
         typewriter_print("Congratulations! You have completed the quest and obtained the luminescent mushroom.")
-        
+        time.sleep(1)  # Slight pause before transitioning
+        clear_screen()
         # Transition to Act 3 for the forest story
         act3_forest()
 
     elif path_choice == "dark":
+        clear_screen()
         typewriter_print("You choose the dark path, but it leads you into a treacherous maze of thorns and dangers.")
         typewriter_print("Unable to find your way out, you succumb to the darkness.")
         typewriter_print("Game over.")
         # Should game end here or allow option to restart?
 
     else:
+        clear_screen()
         typewriter_print("Invalid choice. Please choose either 'illuminated' or 'dark'.")
 
 # Act 3: The Final Confrontation
@@ -95,7 +103,52 @@ def act3_forest():
 
 def act2_beach():
     typewriter_print("Act 2: The Secret Message\n")
-    # Beach story
+    typewriter_print("You follow the path that leads to the beach, drawn to the soothing sound of crashing waves.")
+    typewriter_print("As you walk along the sandy shore, a glimmer catches your eye.")
+    typewriter_print("You discover a small, glass bottle washed up on the beach, containing a tightly rolled piece of paper inside.")
+    typewriter_print("With intrigue, you unroll the paper to reveal a riddle:")
+    typewriter_print("'I speak without a mouth and hear without ears. I have no body, but I come alive with wind. What am I?'")
+
+    # Riddle answer and reward keyword
+    riddle_answer = "echo"
+    reward_keyword = "serenity"
+
+    while True:
+        user_answer = input("Can you solve the riddle? Enter your answer: ")
+
+        if user_answer.lower() == riddle_answer:
+            clear_screen()
+            typewriter_print("Congratulations! You've solved the riddle!")
+            typewriter_print("As the answer escapes your lips, the water before you begins to stir.")
+            typewriter_print("You watch in awe as the water forms into sparkling letters, spelling out 'serenity'.")
+            typewriter_print("The water then recedes, revealing a magical portal shimmering in the distance.")
+            typewriter_print("Before entering the portal, you take a moment to memorize the word 'serenity'.")
+            time.sleep(3)
+            clear_screen()
+            act3_beach(reward_keyword)
+            break
+
+        else:
+            clear_screen()
+            typewriter_print("That's not the correct answer.")
+            choice = input("Do you want to try again? (yes/no) ")
+
+            while choice.lower() not in ["yes", "no"]:
+                clear_screen()
+                typewriter_print("Invalid choice. Please enter 'yes' or 'no'.")
+                choice = input("Do you want to try again? (yes/no) ")
+
+            if choice.lower() == "no":
+                clear_screen()
+                typewriter_print("You decide not to attempt the riddle any further.")
+                typewriter_print("As you continue to relax in the ocean, a massive sea creature rises from the depths and devours you.")
+                typewriter_print("Your adventure ends here, consumed by the unknown perils of the deep sea.")
+                sys.exit(0)
+
+def act3_beach(reward_keyword):
+    typewriter_print("Act 3: The Water Portal\n")
+    # Act 3 beach story and gamepley
+
 
 def act2_cave():
     typewriter_print("Act 2: The Hidden Artifact\n")
